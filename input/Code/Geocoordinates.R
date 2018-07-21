8a1a7c640a63a7ee32b7834f35254aa2fec985b6
Claudia <claudia-jena@gmx.de>
2018-07-21 15:27:52 +0200

    Changed data set

--- input/Code/Geocoordinates.R
+++ input/Code/Geocoordinates.R
@@ -25,21 +25,26 @@ lapply(neededPackages, function(x) suppressPackageStartupMessages(
 
 ################################################################################
 # Shapefile Source Germany: http://www.geodatenzentrum.de/geodaten/gdz_rahmen.gdz_div
-# Europe: https://www.arcgis.com/home/item.html?id=6d611f8d87d54227b494d4c3becef6a0
+# Europe: https://www.naturalearthdata.com/downloads/110m-cultural-vectors/
 
 # Set wd to where shape file is loacated
 setwd("/Users/claudiaguenther/Documents/dolores/input/Geodata/vg2500_geo84")
 
 s.ger    <- raster::shapefile("/Users/claudiaguenther/Documents/dolores/input/Geodata/vg2500_geo84/vg2500_bld.shp")
 #s.europe <- raster::shapefile("/Users/claudiaguenther/Documents/dolores/input/Geodata/europe/Europe_coastline_shapefile/Europe_coastline_poly.shp")
-s.europe <- raster::shapefile("/Users/claudiaguenther/Documents/dolores/input/Geodata/europe/world/MyEurope.shp")
+s.world  <- raster::shapefile("/Users/claudiaguenther/Documents/dolores/input/Geodata/europe/ne_110m_admin_0_countries/ne_110m_admin_0_countries.shp")
 
 plot(s.ger)
 proj4string(s.ger) # WSG84 projection
 
+s.europe <- s.world[s.world@data$CONTINENT == "Europe",]
+s.europe <- s.europe[!(s.europe@data$SOVEREIGNT == "Russia"),] #Exclude Russia
+s.europe <- s.europe[!(s.europe@data$SOVEREIGNT == "Iceland"),] #Exclude Iceland
+
+rm(s.world)
+
 plot(s.europe)
 proj4string(s.europe) # WSG84 projection
-
 
 make.grid <- function(starting.lon,starting.lat, lat.spacing, lon.spacing, n.lon){
            
@@ -57,8 +62,8 @@ grid <- make.grid(starting.lon = 6.1, starting.lat = 47.1,
                   lat.spacing = 0.5, lon.spacing = 0.625,  n.lon = 30)
 
 
-grid.europe <- make.grid(starting.lon = -10, starting.lat = 30, 
-                  lat.spacing = 0.5, lon.spacing = 0.625,  n.lon = 100)
+grid.europe <- make.grid(starting.lon = -10, starting.lat = 35, 
+                  lat.spacing = 1, lon.spacing = 1.25,  n.lon = 100)
 
 # Transform grid to spatial data
 grid        <- SpatialPoints(grid, proj4string = CRS(proj4string(s.ger)))
