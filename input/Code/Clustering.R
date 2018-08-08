ed75ac77fb8a6cdec8b37c468c09c10bbed10c23
Claudia <claudia-jena@gmx.de>
2018-08-08 20:41:13 +0200

    Added summary stats for cluster + clean up

--- input/Code/Clustering.R
+++ input/Code/Clustering.R
@@ -97,7 +97,7 @@ dat.germany.pv   <- data.frame(rbind(c(0, NA, final.memb), dat.germany.pv))
 # Check suitability of cluster solution
 
 # Silouette plot: Check suitability of clustering
-sil <- silhouette(final.memb ,dist =  Dis.ecl)
+sil <- cluster::silhouette(final.memb, dist =  Dis.ecl)
 plot(sil, col=1:2, border=NA)
 
 ################################################################################
@@ -119,99 +119,16 @@ ggplot(check[-1,]) + geom_point(aes(x=lon, y=lat, colour=as.factor(cluster)))
 clheatmap(as.matrix(Dis.ecl))
 
 ################################################################################
-## Check further alternative clustering ways
-fviz_nbclust(dat.germany.tr, FUN = hcut, method = "silhouette")
-fviz_nbclust(dat.germany.tr, FUN = hcut, method = "wss")
+# Add cluster descriptive statistics
 
-
-#library("NbClust")
-#nb <- NbClust(dat.final, distance = "euclidean", min.nc = 2,
-#              max.nc = 10, method = "kmeans")
-
-#library("factoextra")
-#fviz_nbclust(nb)
-
-d <- as.matrix(Dis.ecl)
-heatmap(d, symm = TRUE, scale = "none")
-
-levelplot(d[1:ncol(d),ncol(d):1])
+mean.avail.vec   <- percent(apply(cluster.k$centers, 1, mean))
+mean.av.wind.vec <- percent(apply(cluster.k$centers[,1:8760], 1, mean))
+mean.av.pv.vec   <- percent(apply(cluster.k$centers[,8761:17520], 1, mean))
+sd.vec           <- apply(cluster.k$centers, 1, sd)
+size.vec         <- cluster.k$size
 
 ################################################################################
-## Cluster visualization
 
-final.centers$hours <- 1:nrow(final.centers)
-xyplot(X1  + X2 ~ hours, final.centers, type = "l")
-summary(final.centers)
-apply(final.centers, 2, sd)
-
-
-
-# Clear all plots
-if(!is.null(dev.list())) dev.off()
-
-plot(y=balancing.ex$X1, x=balancing.ex$hours, ylim=c(0,1.1*max(balancing.ex$X2)),
-        col='blue', type='l',
-         main='Spatial balancing of wind volatility', xlab='hours', ylab='Availability / Load',
-         xaxt='n', yaxt='n')
-     points(y=balancing.ex$X2, x=balancing.ex$hours, col='darkblue', type='l', lwd=1)
- axis(2, pretty(c(0, 1.1*max(balancing.ex$X2))), col='blue')
-par(new=T)
- plot(y=balancing.ex$demand, x=balancing.ex$hours, ylim=c(0,1.1*max(balancing.ex$demand)),
-      col='red', type='l',
-      main='Spatial balancing of wind volatility', xlab='hours', ylab='Availability / Load',
-      xaxt='n', yaxt='n')
-axis(4, pretty(c(0, 1.1*max(balancing.ex$demand))), col='red')
-
-################################################################################
-## Compare with current wind generation
-ger.wind = read.csv("/Users/claudiaguenther/Documents/Studium/MEMS/SS2018/EnergyInformatics/ninja_wind_country_DE_current-merra-2_corrected.csv")
-ger.wind.2014 = as.data.frame(t(ger.wind[298035:306794,c(1,4)]))
-colnames(ger.wind.2014) = ger.wind.2014[1,]
-ger.wind.2014  = ger.wind.2014[2,]
-ger.wind.2014  = as.data.frame(apply(ger.wind.2014, 2, as.numeric))
-
-ger.demand.2014 = read.csv("/Users/claudiaguenther/Documents/Studium/MEMS/SS2018/EnergyInformatics/demand_ger_2014.csv")
-ger.demand.2014 = as.data.frame(t(apply(ger.demand.2014, 1, as.numeric)))
-
-# Check alignment of reproduce wind infeed -> coefficients represent capacity per cluster
-# Compare to final centroids
-dat.comp <- as.data.frame((cbind(final.centers, wind = ger.wind.2014[-c(8759:8760),])))
-
-reg.model <- lmMod <- lm(wind ~ X1 + X2 + X3 + X4, data=dat.comp)  
-summary(reg.model)
-
-test <- reg.model$coefficients[2]*dat.comp$X1 + reg.model$coefficients[4]*dat.comp$X2 + 
-         reg.model$coefficients[4]*dat.comp$X4 + reg.model$coefficients[5]*dat.comp$X4
-
-matching <- data.frame(cbind(ger.wind.2014[1:8758,], test, 1:8758))
-xyplot(V1  + test  ~ V3, matching, type = "l")
-
-################################################################################
-## Extended balancing example for wind 2014
-# Clear all plots
-if(!is.null(dev.list())) dev.off()
-
-library(latticeExtra)
-balancing.ex <- cbind(final.centers[6840:7008,c(1:3)], demand = t(ger.demand.2014[6840:7008]))
-obj1 <- xyplot(X1  + X2  ~ hours, balancing.ex, type = "l")
-obj1
-obj2 <- xyplot(demand ~ hours, balancing.ex, type = "l")
-
-doubleYScale(obj1, obj2, add.ylab2 = TRUE, style1 = 1, style2 = 2)
-
-plot(y=balancing.ex$X1, x=balancing.ex$hours, ylim=c(0,1.1*max(balancing.ex$X2)),
-     col='blue', type='l',
-     main='Spatial balancing of wind volatility', xlab='hours', ylab='Availability / Load',
-     xaxt='n', yaxt='n')
-points(y=balancing.ex$X2, x=balancing.ex$hours, col='darkblue', type='l', lwd=1)
-axis(2, pretty(c(0, 1.1*max(balancing.ex$X2))), col='blue')
-par(new=T)
-plot(y=balancing.ex$demand, x=balancing.ex$hours, ylim=c(0,1.1*max(balancing.ex$demand)),
-     col='red', type='l',
-     main='Spatial balancing of wind volatility', xlab='hours', ylab='Availability / Load',
-     xaxt='n', yaxt='n')
-axis(4, pretty(c(0, 1.1*max(balancing.ex$demand))), col='red')
-axis(1, labels = TRUE)
 
 
 
